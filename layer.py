c21a5207d6f66553e3ad5326829f4dd7f06b1f2a
Darian Sereno <dasereno@student.42.fr>
2024-02-05 17:36:57 +0100

    little fix

--- layer.py
+++ layer.py
@@ -3,8 +3,10 @@ import numpy as np
 
 class Layer(ABC):
 	def __init__(self, n_inputs, n_neurons):
-		self.weitghs = np.random.randn(n_inputs, n_neurons)
+		self.weigths = 0.10 * np.random.randn(n_inputs, n_neurons)
 		self.biases = np.zeros((1, n_neurons))
+
+		# exemple : On a 2 inputs, et 3 neurones : weights : [[2.1, 0.3, 0.56], [1.32, 0.56, 3.21]], biais : [0.32, 0.67, 1.56]
 
 	@abstractmethod
 	def forward(self):
@@ -14,4 +16,4 @@ class Layer_Dense(Layer):
 	def __init__(self, n_inputs, n_neurons):
 		super().__init__(n_inputs, n_neurons)
 	def forward(self, inputs):
-		self.output = np.dot(inputs, self.weitghs) + self.biases	
+		self.output = np.dot(inputs, self.weigths) + self.biases	
--- neural_network.py
+++ neural_network.py
@@ -5,14 +5,14 @@ from loss import Loss_CategoricalCrossEntropy
 import matplotlib.pyplot as plt
 from spiral_data import spiral_data
 
-np.random.seed(0)
+np.random.seed(1)
 
 X, y = spiral_data(100, 3)
 
-dense1 = Layer_Dense(2, 3)
+dense1 = Layer_Dense(2, 5)
 activation1 = Activation_ReLU()
 
-dense2 = Layer_Dense(3, 3)
+dense2 = Layer_Dense(5, 3)
 activation2 = Activation_Softmax()
 
 dense1.forward(X)
@@ -26,7 +26,6 @@ loss = loss_function.calculate(activation2.output, y)
 prediction = np.argmax(activation2.output, axis=1)
 
 accuracy = np.mean(prediction==y)
-
 
 print("accuracy :" , accuracy)
 
