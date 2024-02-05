f8a3df6e23f9b326f5b90dd4008bf19baf819bb3
Darian Sereno <dasereno@student.42.fr>
2024-02-05 13:26:47 +0100

    added some annotation

--- activation.py
+++ activation.py
@@ -16,5 +16,12 @@ class Activation_Sigmoid(Activation):
 	
 class Activation_Softmax(Activation):
 	def forward(self, inputs):
+		# on prend tout les inputs (matrice de vecteur ex : [[1, 2, 3], [2, 1, 3], [3, 1, 2]])
+		# pour chaque vecteur on soustrais la valeur maximal ex : [[1 - 3, 2 - 3, 3 - 3], [2 - 3, 1 - 3, 3 - 3], [3 - 3, 1 - 3, 2 - 3]]
+		# puis on passe le result en exponentiel. Donc pas de valeur négative (pas logique) et l'exponentiel
+		# est monotonique, ce qui permet de garder une cohérence de valeur : + la valeur est petite (negative ou proche de zero)
+		# et + elle sera proche de zero, et + la valeur est grande + elle sera grande.
 		exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+		# Probleme : L'exponentiel grandi bien trop vite, donc on normalize les valeurs entre 0 - 1. En divisant ça par la somme de toutes les valeurs
+		# exponentiel.
 		self.output = exp_values / np.sum(exp_values, axis=1, keepdims=True)
--- loss.py
+++ loss.py
@@ -8,7 +8,7 @@ class Loss(ABC):
 
 	def calculate(self, output, y):
 		sample_losses = self.forward(output, y)
-		data_loss = np.mean(sample_losses)
+		data_loss = np.mean(sample_losses) # calcul la moyenne de la loss
 		return data_loss
 
 class Loss_CategoricalCrossEntropy(Loss):
@@ -16,11 +16,15 @@ class Loss_CategoricalCrossEntropy(Loss):
 		samples = len(y_pred)
 		y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
 
-		#print(y_true)
-		if len(y_true.shape) == 1:
+		if len(y_true.shape) == 1: # quand on reçoit un vecteur de label : [0, 2, 0, 1] -> la classe 0, 2, 0, 1 sont vrai.
+			# donc on cherche directement a la case des predictions les valeurs
 			correct_confidences = y_pred_clipped[range(samples), y_true]
-		elif len(y_true.shape) == 2:
+		elif len(y_true.shape) == 2: # One hot encoding, on reçoit une matrice : [[1, 0, 0], [0, 0, 1], [1, 0, 0], [0, 1, 0]]
+			# donc on a juste a multiplier chaque vecteur par la reponse : [1, 0, 0] * 0 = 0, [0, 0, 1] * 2 = 2.... 
+			#(meme resultat que pour la ligne au dessus)
 			correct_confidences = np.sum(y_pred_clipped * y_true, axis=1)
 		
-		negative_log_likelihoods = np.log(correct_confidences)
+		negative_log_likelihoods = -np.log(correct_confidences)
+		# ici on fout au -logarithme (naturel !! base E) les resultat. Ca permet de pouvoir revenir au resultat en 
+		# mettant en exponentiel le logarithme. (pratique pour la backpropagation et l'optimisation)
 		return negative_log_likelihoods
